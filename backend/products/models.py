3c0c4266492f6a133bd96fe95daa511275b3acbe
samborka <henriquealves@onsign.tv>
2025-02-20 11:48:12 -0300

    Create products app

---
+++ backend/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Product(models.Model):
+  name = models.CharField(max_length=255, unique=True)
+  description = models.TextField(blank=True)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  image = models.ImageField(upload_to="product_images/", blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
